06bfa77bda27d266f54c1864dd3ebf95a416a12e
iamFredMian <fredmian3@gmail.com>
2025-03-11 12:20:37 -0500

    updated meta description

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "83086e1ad8780bf5"; }
+/******/ 		__webpack_require__.h = function() { return "32f57304736a805a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
